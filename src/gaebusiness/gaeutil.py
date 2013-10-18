f48d1c1b0d61c8fbe7c4a989d3f40aa9cfeee96d
renzon <renzo.n@gmail.com>
2013-10-18 04:57:22 -0300

    close #

--- src/gaebusiness/gaeutil.py
+++ src/gaebusiness/gaeutil.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import urllib
-from google.appengine.api import urlfetch, taskqueue
+from google.appengine.api import urlfetch, taskqueue, memcache
 from google.appengine.api.taskqueue import Task
 from google.appengine.api.taskqueue import Queue
+from google.appengine.ext import ndb
+from google.appengine.ext.ndb.query import Cursor
 from gaebusiness.business import Command
 
 
 class UrlFetchCommand(Command):
-    def __init__(self, url, params={}, method=urlfetch.GET, headers={}, validate_certificate=True,deadline=30,**kwargs):
+    def __init__(self, url, params={}, method=urlfetch.GET, headers={}, validate_certificate=True, deadline=30,
+                 **kwargs):
         super(UrlFetchCommand, self).__init__(**kwargs)
         self.method = method
         self.headers = headers
         self.validate_certificate = validate_certificate
         self.url = url
         self.params = None
-        self.deadline=deadline
+        self.deadline = deadline
         if params:
             encoded_params = urllib.urlencode(params)
             if method in (urlfetch.POST, urlfetch.PUT, urlfetch.PATCH):
@@ -45,10 +48,44 @@ class TaskQueueCommand(Command):
 
     def set_up(self):
         self._rpc = taskqueue.create_rpc()
-        q=Queue(self._queue_name)
+        q = Queue(self._queue_name)
         q.add_async(self._task, rpc=self._rpc)
 
     def do_business(self, stop_on_error=False):
         self._rpc.get_result()
+
+
+class ModelSearchCommand(Command):
+    def __init__(self, query, page_size=100, start_cursor=None, use_cache=True, **kwargs):
+        self.__future = None
+        self.__cached_keys = None
+        self.cursor = None
+        self.more = None
+        if isinstance(start_cursor, basestring):
+            start_cursor = Cursor(urlsafe=start_cursor)
+        super(ModelSearchCommand, self).__init__(query=query, page_size=page_size, start_cursor=start_cursor,
+                                                 use_cache=use_cache,
+                                                 **kwargs)
+
+    def set_up(self):
+        if self.use_cache and self.start_cursor:
+            try:
+                cached_tuple = memcache.get(self.start_cursor.urlsafe())
+                if cached_tuple:
+                    self.__cached_keys, self.cursor, self.more = cached_tuple[0], cached_tuple[1], True
+            except:
+                pass
+        if not self.__cached_keys:
+            self.__future = self.query.fetch_page_async(self.page_size, start_cursor=self.start_cursor, keys_only=True)
+
+    def do_business(self, stop_on_error=True):
+        if self.__future:
+            model_keys, self.cursor, self.more = self.__future.get_result()
+            future = ndb.get_multi_async(model_keys)
+            if self.use_cache and self.start_cursor and len(model_keys) == self.page_size:
+                memcache.set(self.start_cursor.urlsafe(), (model_keys, self.cursor))
+            self.result = [f.get_result() for f in future]
+        else:
+            self.result = ndb.get_multi(self.__cached_keys)
 
 
--- test/gaeutil_tests.py
+++ test/gaeutil_tests.py
@@ -2,10 +2,12 @@
 from __future__ import absolute_import, unicode_literals
 import unittest
 import urllib
-from google.appengine.api import urlfetch
+from google.appengine.api import urlfetch, memcache
+from google.appengine.ext import ndb
 from gaebusiness import gaeutil
-from gaebusiness.gaeutil import UrlFetchCommand, TaskQueueCommand
+from gaebusiness.gaeutil import UrlFetchCommand, TaskQueueCommand, ModelSearchCommand
 from mock import Mock
+from util import GAETestCase
 
 
 class UrlfecthTests(unittest.TestCase):
@@ -62,3 +64,65 @@ class TaskQueueTests(unittest.TestCase):
         task_cls.assert_called_once_with(url=url, params=params)
         queue_obj.add_async.assert_called_once_with(task_obj, rpc=rpc_mock)
         rpc_mock.get_result.assert_called_once_with()
+
+
+# Stub used for next tests
+class SomeModel(ndb.Model):
+    index = ndb.IntegerProperty()
+
+    @classmethod
+    def query_index_ordered(cls):
+        return cls.query().order(cls.index)
+
+
+class ModelSearchCommandTests(GAETestCase):
+    def test_cache(self):
+        ndb.put_multi([SomeModel(index=i) for i in xrange(7)])
+        #asserting nothing is cached for the first results
+        cursor = ModelSearchCommand(SomeModel.query_index_ordered(), 3).execute().cursor
+        self.assertIsNone(memcache.get(cursor.urlsafe()))
+
+        #asserting nothing is cached when using arg use_cache=False
+        cursor2 = ModelSearchCommand(SomeModel.query_index_ordered(), 3, cursor, use_cache=False).execute().cursor
+        self.assertIsNone(memcache.get(cursor.urlsafe()))
+
+        #asserting items are cached
+        ModelSearchCommand(SomeModel.query_index_ordered(), 3, cursor).execute()
+        cached_model_keys, cached_cursor = memcache.get(cursor.urlsafe())
+        cached_models = ndb.get_multi(cached_model_keys)
+        self.assertListEqual(list(xrange(3, 6)), [some_model.index for some_model in cached_models])
+        self.assertEqual(cursor2, cached_cursor)
+
+        #asserting cached is used
+        cmd = ModelSearchCommand(SomeModel.query_index_ordered(), 3, cursor).execute()
+        self.assertIsNone(cmd._ModelSearchCommand__future)
+        self.assertIsNotNone(cmd._ModelSearchCommand__cached_keys)
+        self.assertListEqual(list(xrange(3, 6)), [some_model.index for some_model in cmd.result])
+
+
+        #asserting results are not cached if len of results are less then search page_size
+        ModelSearchCommand(SomeModel.query_index_ordered(), 3, cursor2).execute()
+        self.assertIsNone(memcache.get(cursor2.urlsafe()))
+
+
+    def test_cursor_search(self):
+        ndb.put_multi([SomeModel(index=i) for i in xrange(8)])
+
+        #Asserting cursor works
+        search = ModelSearchCommand(SomeModel.query_index_ordered(), 3).execute()
+        self.assertTrue(search.more)
+        cursor = search.cursor
+        self.assertIsNotNone(cursor)
+        self.assertListEqual(list(xrange(3)), [some_model.index for some_model in search.result])
+
+        search = ModelSearchCommand(SomeModel.query_index_ordered(), 3, cursor).execute()
+        self.assertTrue(search.more)
+        cursor2 = search.cursor
+        self.assertIsNotNone(cursor2)
+        self.assertListEqual(list(xrange(3, 6)), [some_model.index for some_model in search.result])
+
+        search = ModelSearchCommand(SomeModel.query_index_ordered(), 3, cursor2.urlsafe()).execute()
+        cursor3 = search.cursor
+        self.assertIsNotNone(cursor2)
+        self.assertListEqual(list(xrange(6, 8)), [some_model.index for some_model in search.result])
+
